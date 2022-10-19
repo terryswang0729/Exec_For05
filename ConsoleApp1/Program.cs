e4ba5d795a5605bd65b0a89d0d0229d8830843f7
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 16:58:49 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			for (int i = 1; i<= 5; i++)
+			{
+				string row = new string('+', (5 - i));
+				Console.Write(row);
+				for (int k = 1; k <= i; k++)
+				{
+					Console.Write(i);
+				}
+				Console.WriteLine();
+			}
+			
+		}
+	}
+}
---
+++ Exec_For05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For05
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			for (int i = 1; i <= 4; i++)
+			{
+
+				Console.WriteLine(i);
+			}
+		}
+	}
+}
